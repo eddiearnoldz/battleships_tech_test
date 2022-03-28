cb4a7ad3756a19c8700165ceca08e971fd6cf5dd
Eddie Arnold <eddiearnoldz@me.com>
2022-03-28 20:31:56 +0100

    players can set up both boards but game not fully implemented yet. Need to start game and define player turn to rotate grids

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const rl = readline.createInterface({input: process.stdin,output: process.stdout
 
 const grid1 = new Grid
 const grid2 = new Grid
-const shipPosition = (type, size, example, grid) => {
+const shipPosition = (type, size, example, grid, player) => {
   return new Promise((resolve, reject) => {
-    rl.question(`On which coordinates would you like to place the your ${type}? e.g ${example}\n`, (answer) => {
-      const Position = answer
-      grid.ships.push(new Battleship(size, [Position]))
-      grid.positionShip(new Battleship(size, [Position]))
+    rl.question(`${player}, on which coordinates would you like to place the your ${type}? e.g ${example}\n`, (answer) => {
+      grid.ships.push(new Battleship(size, [answer]))
+      grid.positionShip(new Battleship(size, [answer]))
       console.table(grid.getCurrentState())
       resolve()
     })
@@ -23,12 +22,17 @@ const renderStartingBoard = (player,grid) =>{
 
 const game = async () => {
   renderStartingBoard('Player 1', grid1)
-  await shipPositionPlayer1('Destroyer', 2, 'A1, A2', grid1)
-  await shipPositionPlayer1('Submarine', 3, 'A1, A3', grid1)
-  await shipPositionPlayer1('Cruiser', 3, 'A1, A3', grid1)
-  await shipPositionPlayer1('Battleship', 4, 'A1, A4', grid1)
-  await shipPositionPlayer1('Carrier', 5, 'A1, A5', grid1)
+  await shipPosition('Destroyer', 2, 'A1, A2', grid1, 'Player 1')
+  await shipPosition('Submarine', 3, 'A1, A3', grid1, 'Player 1')
+  await shipPosition('Cruiser', 3, 'A1, A3', grid1, 'Player 1')
+  await shipPosition('Battleship', 4, 'A1, A4', grid1, 'Player 1')
+  await shipPosition('Carrier', 5, 'A1, A5', grid1, 'Player 1')
   renderStartingBoard('Player 2', grid2)
+  await shipPosition('Destroyer', 2, 'A1, A2', grid2, 'Player 2')
+  await shipPosition('Submarine', 3, 'A1, A3', grid2, 'Player 2')
+  await shipPosition('Cruiser', 3, 'A1, A3', grid2, 'Player 2')
+  await shipPosition('Battleship', 4, 'A1, A4', grid2, 'Player 2')
+  await shipPosition('Carrier', 5, 'A1, A5', grid2, 'Player 2')
 }
 
 game()
