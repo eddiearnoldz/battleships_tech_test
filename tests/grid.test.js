91293654cb44f2c8fb6b4af0702853a059f0c151
Eddie Arnold <eddiearnoldz@me.com>
2022-03-28 13:28:44 +0100

    works for other size boats too. Now to populate the inbetween of the ship

--- tests/grid.test.js
+++ tests/grid.test.js
@@ -24,9 +24,9 @@ describe('Grid class', () => {
       'J': ['-','-','-','-','-','-','-','-','-','-'],
   }
      battleship2Double = new Battleship(2, 1, ['A1', 'A2'] )
-     battleship3Double = new Battleship(3, 1, ['B1', 'B4'] )
-     battleship4Double = new Battleship(4, 1, ['C1', 'C5'] )
-     battleship5Double = new Battleship(5, 1, ['D1', 'D6'] )
+     battleship3Double = new Battleship(3, 1, ['B1', 'B3'] )
+     battleship4Double = new Battleship(4, 1, ['C1', 'C4'] )
+     battleship5Double = new Battleship(5, 1, ['D1', 'D5'] )
   });
 
   it('has an array of nested arrays that store the state of the game', () => {
@@ -41,5 +41,15 @@ describe('Grid class', () => {
     battleship2Double.populateShip(battleship2Double.size)
     grid.positionEndOfShip(battleship2Double)
     expect(grid.getCurrentState()['A']).toEqual(['-','O','-','-','-','-','-','-','-','-'])
+  })
+  it('can change the start position of a 4-boat on the grid to an O', () => {
+    battleship4Double.populateShip(battleship4Double.size)
+    grid.positionStartOfShip(battleship4Double)
+    expect(grid.getCurrentState()['C']).toEqual(['O','-','-','-','-','-','-','-','-','-'])
+  })
+  it('can change the end position of a 4-boat on the grid to an O', () => {
+    battleship2Double.populateShip(battleship4Double.size)
+    grid.positionEndOfShip(battleship4Double)
+    expect(grid.getCurrentState()['C']).toEqual(['-','-','-','O','-','-','-','-','-','-'])
   })
 })
